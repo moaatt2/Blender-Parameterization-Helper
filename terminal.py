2d34f2e83f69ac2811c0d15ab2743e1e1b39cd98
Dakota McKay <dakotamckay72@gmail.com>
2025-08-12 17:59:44 -0400

    Move parameterization code into a function.

--- terminal.py
+++ terminal.py
@@ -23,246 +23,248 @@ print("I will take the blender code from your clipboard, create a paramterized v
 input("Press Enter When you are ready to start:")
 clear_terminal()
 
-test_code = pyperclip.paste()
+code = pyperclip.paste()
 
-################################
-### Split code into sections ###
-################################
+def parameterize_blender_code(code):
 
-# Set up section containers
-section_mapping = {
-    "Imports":   "",
-    "Parameters": "",
-    "Model Code": "",
-}
+    ################################
+    ### Split code into sections ###
+    ################################
 
-# Itterate over lines and split into sections
-current_section = None
-for line in test_code.split('\n'):
+    # Set up section containers
+    section_mapping = {
+        "Imports":   "",
+        "Parameters": "",
+        "Model Code": "",
+    }
 
-    # Check for section headers
-    if ("### " in line) and (" ###" in line):
-        current_section = line.replace("#","").strip()
+    # Itterate over lines and split into sections
+    current_section = None
+    for line in code.split('\n'):
 
-    # Itterate over lines in a target section
-    if current_section in section_mapping:
+        # Check for section headers
+        if ("### " in line) and (" ###" in line):
+            current_section = line.replace("#","").strip()
 
-        # Skip useless lines
-        if line.strip() == "": continue # empty lines
-        if line.startswith("#"): continue # comments
+        # Itterate over lines in a target section
+        if current_section in section_mapping:
 
-        # Add line to the current section
-        section_mapping[current_section] += line + "\n"
+            # Skip useless lines
+            if line.strip() == "": continue # empty lines
+            if line.startswith("#"): continue # comments
 
-
-########################
-### Parse Parameters ###
-########################
-
-# Itterate over lines and parse paraemters
-parameters = dict()
-for line in section_mapping["Parameters"].split('\n'):
-
-    # Determine if line is a parameter
-    if "=" in line:
-
-        # Get Values from lines
-        split_vals = line.split("=")
-        name, value = map(str.strip, split_vals)
-
-        # Check type of value
-        value_type = None
-        if '"' in  value or "'" in value:
-            value_type = "str"
-        elif value.replace('.', '', 1).isdigit():
-            value_type = "number"
-        elif "radians" in value:
-            value_type = "angle"
-            value = value.replace("radians(", "").replace(")", "").replace("math.", "").strip()
-            value = f"{value}/180 * math.pi"  # Convert to radians
-        else:
-            value_type = "Unknown"
-
-        parameters[name] = [value, value_type]
+            # Add line to the current section
+            section_mapping[current_section] += line + "\n"
 
 
-#############################
-### Add imports to output ###
-#############################
+    ########################
+    ### Parse Parameters ###
+    ########################
 
-# Initialze output
-output = """
+    # Itterate over lines and parse paraemters
+    parameters = dict()
+    for line in section_mapping["Parameters"].split('\n'):
+
+        # Determine if line is a parameter
+        if "=" in line:
+
+            # Get Values from lines
+            split_vals = line.split("=")
+            name, value = map(str.strip, split_vals)
+
+            # Check type of value
+            value_type = None
+            if '"' in  value or "'" in value:
+                value_type = "str"
+            elif value.replace('.', '', 1).isdigit():
+                value_type = "number"
+            elif "radians" in value:
+                value_type = "angle"
+                value = value.replace("radians(", "").replace(")", "").replace("math.", "").strip()
+                value = f"{value}/180 * math.pi"  # Convert to radians
+            else:
+                value_type = "Unknown"
+
+            parameters[name] = [value, value_type]
+
+
+    #############################
+    ### Add imports to output ###
+    #############################
+
+    # Initialze output
+    output = """
 ###############
 ### Imports ###
 ###############
 
 """
 
-# Check for bpy import
-imports_bpy = False
-for line in section_mapping["Imports"].split('\n'):
-    if line.strip() == "import bpy":
-        imports_bpy = True
+    # Check for bpy import
+    imports_bpy = False
+    for line in section_mapping["Imports"].split('\n'):
+        if line.strip() == "import bpy":
+            imports_bpy = True
 
-# Import bpy if not present
-if not imports_bpy:
-    section_mapping["Imports"] = "import bpy\n" + section_mapping["Imports"]
+    # Import bpy if not present
+    if not imports_bpy:
+        section_mapping["Imports"] = "import bpy\n" + section_mapping["Imports"]
 
-# Check for import of math library
-imports_math = False
-for line in section_mapping["Imports"].split('\n'):
-    if line.strip() == "import math":
-        imports_math = True
+    # Check for import of math library
+    imports_math = False
+    for line in section_mapping["Imports"].split('\n'):
+        if line.strip() == "import math":
+            imports_math = True
 
-# Import math if not present
-if not imports_math:
-    section_mapping["Imports"] = "import math\n" + section_mapping["Imports"]
+    # Import math if not present
+    if not imports_math:
+        section_mapping["Imports"] = "import math\n" + section_mapping["Imports"]
 
-# Add other user defined imports
-output += section_mapping["Imports"] + "\n\n"
-
-
-##################################
-### Set Weave Name If Provided ###
-##################################
-
-weave_name = parameters.get("weave_name")
-if weave_name is not None:
-    weave_name = weave_name[0]
-    output += "################\n### Settings ###\n################\n\n# Weave Name\n"
-    output += f"weave_name = {weave_name}\n\n\n"
+    # Add other user defined imports
+    output += section_mapping["Imports"] + "\n\n"
 
 
-####################################
-### Create Model Update Function ###
-####################################
+    ##################################
+    ### Set Weave Name If Provided ###
+    ##################################
 
-# Add section header
-output += "#############################\n### Model Update Function ###\n#############################\n\n"
-
-# Add function definition
-output += "def update_model(self, context):\n\n"
-
-# Add clean up section
-output += "\t################\n\t### Clean Up ###\n\t################\n\n"
-output += "\t# Clear Scene - Delete all Objects\n"
-output += "\tbpy.ops.object.select_all(action='DESELECT')\n"
-output += "\tbpy.ops.object.select_by_type(type='MESH')\n"
-output += "\tbpy.ops.object.delete()\n\n"
-
-# Add Code  header
-output += "\t####################\n\t### Create Rings ###\n\t####################\n\n"
-
-# Replace parameters in model code with self.<parameter_name>
-model_code = section_mapping["Model Code"]
-for key in parameters:
-    model_code = model_code.replace(key, f"self.{key}")
-
-# Clean up cases where "self.self." could appear (if one parameter name is a substring of another e.g. "angle_1" and "angle_10")
-while "self.self." in model_code:
-    model_code = model_code.replace("self.self.", "self.")
-
-# Prepend tabs and add to output
-for line in model_code.split('\n'):
-    output += "\t" + line + "\n"
+    weave_name = parameters.get("weave_name")
+    if weave_name is not None:
+        weave_name = weave_name[0]
+        output += "################\n### Settings ###\n################\n\n# Weave Name\n"
+        output += f"weave_name = {weave_name}\n\n\n"
 
 
-###################################
-### Define Custom Menu Contents ###
-###################################
+    ####################################
+    ### Create Model Update Function ###
+    ####################################
 
-# Add Section Heading
-output += "\n\n############################\n### Define Menu Contents ###\n############################\n\n"
+    # Add section header
+    output += "#############################\n### Model Update Function ###\n#############################\n\n"
 
-# Add class definition
-output += "class ChainmailProperties(bpy.types.PropertyGroup):\n"
+    # Add function definition
+    output += "def update_model(self, context):\n\n"
 
-# Add properties for each parameter
-for key, value in parameters.items():
-    if key != "weave_name":
+    # Add clean up section
+    output += "\t################\n\t### Clean Up ###\n\t################\n\n"
+    output += "\t# Clear Scene - Delete all Objects\n"
+    output += "\tbpy.ops.object.select_all(action='DESELECT')\n"
+    output += "\tbpy.ops.object.select_by_type(type='MESH')\n"
+    output += "\tbpy.ops.object.delete()\n\n"
 
-        # Create clean name for display
-        clean_name = key.replace("_", " ").capitalize()
+    # Add Code  header
+    output += "\t####################\n\t### Create Rings ###\n\t####################\n\n"
 
-        # Create property
-        output += f"\t{key}: bpy.props.FloatProperty(\n"
-        output += f'\t\tname="{clean_name}",\n'
-        output += f'\t\tdefault={value[0]},\n'
+    # Replace parameters in model code with self.<parameter_name>
+    model_code = section_mapping["Model Code"]
+    for key in parameters:
+        model_code = model_code.replace(key, f"self.{key}")
 
-        # Add specific type handling
-        if value[1] == "angle":
-            output += '\t\tsubtype="ANGLE",\n'
-            output += '\t\tunit="ROTATION",\n'
+    # Clean up cases where "self.self." could appear (if one parameter name is a substring of another e.g. "angle_1" and "angle_10")
+    while "self.self." in model_code:
+        model_code = model_code.replace("self.self.", "self.")
 
-        # Close out property definition
-        output += '\t\tupdate=update_model\n'
-        output += "\t)\n"
-
-
-#########################################
-### Create Custom Menu & Add Contents ###
-#########################################
-
-# Add Section Heading
-output += "\n\n###################\n### Create Menu ###\n###################\n\n"
-
-# Add class definition
-output += "class VIEW3D_PT_chainmail_panel(bpy.types.Panel):\n"
-
-# Add weave name
-if "weave_name" in parameters:
-    weave_name = parameters["weave_name"][0].replace('"', '').replace("'", "")
-    output += f'\tbl_label = "{weave_name} Modification Pannel"\n'
-else:
-    output += '\tbl_label = "Chainmail Modification Pannel"\n'
-
-# Finish class variables
-output += "\tbl_idname = 'VIEW3D_PT_chainmail_ring'\n\tbl_space_type = 'VIEW_3D'\n\tbl_region_type = 'UI'\n\tbl_category = 'Chainmail'\n\n"
-
-# Add definition of draw function
-output += "\tdef draw(self, context):\n"
-
-# Add layout & props
-output += "\t\tlayout = self.layout\n"
-output += "\t\tprops = context.scene.chainmail_props\n\n"
-
-# Add item for each parameter
-for key in parameters:
-    if key != "weave_name":
-        output += f'\t\tlayout.prop(props, "{key}")\n'
-
-# Add button to start keyboard controls
-output += "\n\t\tlayout.separator()\n\t\tlayout.operator('wm.chainmail_tweak_modal', text='Keyboard Tweak Mode')\n"
+    # Prepend tabs and add to output
+    for line in model_code.split('\n'):
+        output += "\t" + line + "\n"
 
 
-################################
-### Create Keyboard Controls ###
-################################
+    ###################################
+    ### Define Custom Menu Contents ###
+    ###################################
 
-# Add Section Heading
-output += "\n\n################################\n### Create Keyboard Controls ###\n################################\n\n"
+    # Add Section Heading
+    output += "\n\n############################\n### Define Menu Contents ###\n############################\n\n"
 
-# Add global variable to track selected index
-output += "tweaker_selected_index = 0\n"
+    # Add class definition
+    output += "class ChainmailProperties(bpy.types.PropertyGroup):\n"
 
-# Add Class Definition
-output += "class ChainmailTweaker(bpy.types.Operator):\n\n"
+    # Add properties for each parameter
+    for key, value in parameters.items():
+        if key != "weave_name":
 
-output += '\tbl_idname = "wm.chainmail_tweak_modal"\n\tbl_label  = "Chainmail Tweaker (Arrow Keys + Tab)"\n\n'
+            # Create clean name for display
+            clean_name = key.replace("_", " ").capitalize()
 
-# Add variable names
-output += "\tvar_names = [\n"
-for key in parameters:
-    if key != "weave_name":
-        output += f'\t\t"{key}",\n'
-output += "\t]\n\n"
+            # Create property
+            output += f"\t{key}: bpy.props.FloatProperty(\n"
+            output += f'\t\tname="{clean_name}",\n'
+            output += f'\t\tdefault={value[0]},\n'
+
+            # Add specific type handling
+            if value[1] == "angle":
+                output += '\t\tsubtype="ANGLE",\n'
+                output += '\t\tunit="ROTATION",\n'
+
+            # Close out property definition
+            output += '\t\tupdate=update_model\n'
+            output += "\t)\n"
 
 
-################################
-### Add remaining fixed code ###
-################################
+    #########################################
+    ### Create Custom Menu & Add Contents ###
+    #########################################
 
-output += """
+    # Add Section Heading
+    output += "\n\n###################\n### Create Menu ###\n###################\n\n"
+
+    # Add class definition
+    output += "class VIEW3D_PT_chainmail_panel(bpy.types.Panel):\n"
+
+    # Add weave name
+    if "weave_name" in parameters:
+        weave_name = parameters["weave_name"][0].replace('"', '').replace("'", "")
+        output += f'\tbl_label = "{weave_name} Modification Pannel"\n'
+    else:
+        output += '\tbl_label = "Chainmail Modification Pannel"\n'
+
+    # Finish class variables
+    output += "\tbl_idname = 'VIEW3D_PT_chainmail_ring'\n\tbl_space_type = 'VIEW_3D'\n\tbl_region_type = 'UI'\n\tbl_category = 'Chainmail'\n\n"
+
+    # Add definition of draw function
+    output += "\tdef draw(self, context):\n"
+
+    # Add layout & props
+    output += "\t\tlayout = self.layout\n"
+    output += "\t\tprops = context.scene.chainmail_props\n\n"
+
+    # Add item for each parameter
+    for key in parameters:
+        if key != "weave_name":
+            output += f'\t\tlayout.prop(props, "{key}")\n'
+
+    # Add button to start keyboard controls
+    output += "\n\t\tlayout.separator()\n\t\tlayout.operator('wm.chainmail_tweak_modal', text='Keyboard Tweak Mode')\n"
+
+
+    ################################
+    ### Create Keyboard Controls ###
+    ################################
+
+    # Add Section Heading
+    output += "\n\n################################\n### Create Keyboard Controls ###\n################################\n\n"
+
+    # Add global variable to track selected index
+    output += "tweaker_selected_index = 0\n"
+
+    # Add Class Definition
+    output += "class ChainmailTweaker(bpy.types.Operator):\n\n"
+
+    output += '\tbl_idname = "wm.chainmail_tweak_modal"\n\tbl_label  = "Chainmail Tweaker (Arrow Keys + Tab)"\n\n'
+
+    # Add variable names
+    output += "\tvar_names = [\n"
+    for key in parameters:
+        if key != "weave_name":
+            output += f'\t\t"{key}",\n'
+    output += "\t]\n\n"
+
+
+    ################################
+    ### Add remaining fixed code ###
+    ################################
+
+    output += """
     def __init__(self):
         global tweaker_selected_index
         self.selected_index = tweaker_selected_index
@@ -342,12 +344,14 @@ def unregister():
 
 if __name__ == "__main__":
     register()
-""".replace("    ", "\t")  # Replace spaces with tabs for consistency
+    """.replace("    ", "\t")  # Replace spaces with tabs for consistency
+
+    return output
 
 
-####################
-### Print Output ###
-####################
+#####################
+### Return Output ###
+#####################
 
-pyperclip.copy(output)
+pyperclip.copy(parameterize_blender_code(code))
 print("The updated code is in your clipboard now.")
